e899210634bef06c593d0446d7f5c4e2e5286683
Pop228822 <465485@edu.itmo.ru>
2025-05-15 22:22:12 +0300

    ФИО

--- app/auth.py
+++ app/auth.py
@@ -15,7 +15,7 @@ from app.models import User
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
