330988bafdb94bf70cc288536c1e8228b5e96bd5
Pop228822 <465485@edu.itmo.ru>
2025-06-03 04:43:00 +0300

    если заблочен то нельзя важные действия делать

--- app/auth.py
+++ app/auth.py
@@ -62,6 +62,8 @@ def get_current_user(
     user = db.query(User).filter(User.id == int(user_id)).first()
     if not user:
         raise HTTPException(status_code=401, detail="Пользователь не найден")
+    if user.is_blocked:
+        raise HTTPException(status_code=403, detail="Аккаунт заблокирован.")
     return user
 
 def get_current_user_safe(
