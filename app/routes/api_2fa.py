680e3bd81a4764e909ba8288cfa679ca1928828d
Pop228822 <465485@edu.itmo.ru>
2025-06-20 04:35:53 +0300

    add timer in 2fa

--- app/routes/api_2fa.py
+++ app/routes/api_2fa.py
@@ -1,9 +1,12 @@
 # routers/api_2fa.py
+from random import randint
 
 from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from datetime import datetime
+from datetime import datetime, timedelta
+
+from app.email_utils import send_2fa_code
 from app.models import User
 from app.auth import get_session, create_access_token
 
@@ -31,3 +34,23 @@ def verify_2fa_api(data: TwoFactorRequest, db: Session = Depends(get_session)):
 
     token = create_access_token({"sub": str(user.id)})
     return {"access_token": token}
+
+class EmailRequest(BaseModel):
+    email: str
+
+@router.post("/resend-2fa")
+def resend_2fa(data: EmailRequest, db: Session = Depends(get_session)):
+    user = db.query(User).filter(User.email == data.email).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="user_not_found")
+
+    if not user.is_email_verified or user.is_blocked:
+        raise HTTPException(status_code=403, detail="not_allowed")
+
+    code = str(randint(100000, 999999))
+    user.twofa_code = code
+    user.twofa_expires_at = datetime.utcnow() + timedelta(minutes=5)
+    db.commit()
+    send_2fa_code(user.email, code)
+
+    return {"status": "resent"}
