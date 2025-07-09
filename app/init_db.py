0ea2fd66ba293d1fe6ea2679321433ff8de11abd
Pop228822 <465485@edu.itmo.ru>
2025-07-09 20:05:17 +0300

    init db

--- app/init_db.py
+++ app/init_db.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel
 from app.database import engine # или app.database, если внутри пакета
 
 # Явный импорт всех моделей
-from models import User, Employee, ReputationRecord, LoginAttempt, PendingUser, CheckLog, RateLimit
+from app.models import User, Employee, ReputationRecord, LoginAttempt, PendingUser, CheckLog, RateLimit
 
 def init():
     SQLModel.metadata.create_all(engine)
