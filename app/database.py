fcc0ea3924c786bcf3ce1e7f8968f0bd01e293ee
Pop228822 <465485@edu.itmo.ru>
2025-07-10 01:30:12 +0300

    pool_ping

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from sqlmodel import create_engine, Session
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
 
 def get_session():
     with Session(engine) as session:
